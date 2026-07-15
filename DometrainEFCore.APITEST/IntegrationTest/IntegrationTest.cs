7338a20c8fe1a49c9c1029ab404f27125328e547
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-07-15 07:23:21 +0200

    Win smaprt app letiltotta, de feloldva

--- DometrainEFCore.APITEST/IntegrationTest/IntegrationTest.cs
+++ DometrainEFCore.APITEST/IntegrationTest/IntegrationTest.cs
@@ -1,4 +1,6 @@
 ﻿using Dometrain.EFCore.API.Data;
+using Dometrain.EFCore.API.Models;
+using Microsoft.AspNetCore.DataProtection.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ namespace DometrainEFCore.APITEST.IntegrationTest
 {
     public class IntegrationTest
     {
+        /*
         private readonly MoviesContext _testContext;
         private readonly MoviesContext _verificationContext;
 
@@ -17,19 +20,37 @@ namespace DometrainEFCore.APITEST.IntegrationTest
         {
             var options = new DbContextOptionsBuilder<MoviesContext>()
                 .UseSqlServer("""
-                Data Source=localhost;
-                Initial Catalog=TestDb;
-                User Id=sa;
-                Password=MySaPassword123;
-                TrustServerCertificate=True;
-                """)
+                        Data Source=localhost;
+                        Initial Catalog=TestDb;
+                        User Id=sa;
+                        Password=MySaPassword123;
+                        TrustServerCertificate=True;
+                        """)
                 .Options;
 
             _testContext = new MoviesContext(options);
             _verificationContext = new MoviesContext(options);
 
-            //innen folytatni!!!! 01:56-tól
+            _testContext.Database.EnsureDeleted();
+            _testContext.Database.EnsureCreated();
         }
 
+
+        [Fact]
+        public async Task WhenGenreCreated_GenreIsInDatabase()
+        {
+            var repository = new GenreRepository(_testContext);
+            var genreToCreate = new Genre { Name = "MyAwesomeGenre" };
+            var createdGenre = await repository.Create(genreToCreate);
+
+            Assert.NotNull(createdGenre);
+            Assert.True(createdGenre.Id > 0);
+            var verificationGenre = _verificationContext.Genres.Find(createdGenre.Id);
+            Assert.NotNull(verificationGenre);
+            Assert.Equal(genreToCreate.Name, verificationGenre.Name);
+        }
+
+
+        */
     }
 }
